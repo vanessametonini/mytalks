23e0fbc576b92408abeef70581c566f8e3b11222
Vanessa Me Tonini <vanessametonini@gmail.com>
2016-07-15 16:15:18 -0300

    Ajustando apresentacao fisl 17

---
+++ fisl/17/heroku-confboilerplate/old/config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "styles/"
+sass_dir = "scss"
+images_dir = "images"
+fonts_dir = "fonts/"
+relative_assets = true
+
+output_style = :compressed
+environment = :development
