03f25cb50a3742cb4e34a95a4e0aed1e82e61e19
Vanessa Me Tonini <vanessametonini@gmail.com>
2017-06-01 15:58:49 -0300

    Removed Shower copy. Fixed paths to shower lib.

--- fisl/17/old/config.rb
+++
@@ -1,9 +0,0 @@
-http_path = "/"
-css_dir = "styles/"
-sass_dir = "scss"
-images_dir = "images"
-fonts_dir = "fonts/"
-relative_assets = true
-
-output_style = :compressed
-environment = :development
